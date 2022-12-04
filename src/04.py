b52258f26148ea4aacbb45a6aef7bc54811061e9
Ava Yert <ava@yert.pink>
2022-12-04 09:56:07 +0200

    Add solutions for day 4

---
+++ src/04.py
@@ -0,0 +1,17 @@
+from common import lines
+from itertools import islice
+import re
+
+
+lines = [
+    tuple(int(digit) for digit in re.findall('\d+', line))
+    for line in lines
+]
+
+def chunks(iterable, *, n):
+    it = iter(iterable)
+    return iter(lambda: tuple(islice(it, n)), ())
+
+
+print('Part 1:', sum((a <= x and b >= y) or (x <= a and y >= b) for (a, b, x, y) in lines))
+print('Part 2:', sum(a <= y and x <= b for (a, b, x, y) in lines))
